f75de18b1c4f5374e1f28f94e2a4e2e47c9cf0ea
Cristina Lester <cristina.deelester@gmail.com>
2022-02-05 16:51:46 -0800

    initial commit

---
+++ semaphore/semaphore.js
@@ -0,0 +1,38 @@
+class Semaphore {
+  constructor(maxRunningRequests = 1) {
+    this.maxRunningRequests = maxRunningRequests;
+    this.currentRequests = [];
+    this.runningRequests = 0;
+  }
+
+  callFunction(functionToCall, ...args) {
+    return new Promise((resolve, reject) => {
+      this.currentRequests.push({
+        resolve,
+        reject,
+        functionToCall,
+        args,
+      });
+      this.runNext();
+    });
+  }
+
+  runNex() {
+    if (!this.currentRequests.length) return;
+    const req = this.currentRequests.shift();
+    const { resolve, reject, functionToCall, args } = req;
+    this.runningRequests++;
+    let action = functionToCall(...args);
+    action
+      .then((res) => resolve(res))
+      .catch((err) => reject(err))
+      .finally(() => {
+        this.runningRequests--;
+        this.runNext();
+      });
+  }
+}
+
+let sem = new Semaphore(2);
+
+sem.callFunction("fetch", "https://www.google.com");
