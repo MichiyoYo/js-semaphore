5e1a260fa7e21c8c5ceacae7b676c98890d88c03
Cristina Lester <cristina.deelester@gmail.com>
2022-02-09 17:48:01 -0800

    changed how tasks are called

--- semaphore/semaphore.js
+++ semaphore/semaphore.js
@@ -1,10 +1,19 @@
 class Semaphore {
   constructor(maxRunningRequests = 1) {
+    //limitator
     this.maxRunningRequests = maxRunningRequests;
+    //queue of current requests
     this.currentRequests = [];
+    //requests that are currently running
     this.runningRequests = 0;
   }
 
+  /**
+   * Adds a task to the currentRequest queue
+   * @param {*} functionToCall
+   * @param  {...any} args
+   * @returns
+   */
   callFunction(functionToCall, ...args) {
     return new Promise((resolve, reject) => {
       this.currentRequests.push({
@@ -19,20 +28,35 @@ class Semaphore {
 
   runNext() {
     if (!this.currentRequests.length) return;
+    //extract first request from the queue
     const req = this.currentRequests.shift();
+    //destructure request properties into variables
     const { resolve, reject, functionToCall, args } = req;
+    console.log(req);
+    //increment number of running requests
     this.runningRequests++;
-    let action = functionToCall(...args);
-    action
-      .then((res) => resolve(res))
-      .catch((err) => reject(err))
-      .finally(() => {
-        this.runningRequests--;
-        this.runNext();
-      });
+    //run request asyncronously
+
+    let task = functionToCall;
+    if (typeof task === "function") {
+      task(...args)
+        .then((res) => resolve(res))
+        .catch((err) => reject(err))
+        .finally(() => {
+          this.runningRequests--;
+          this.runNext();
+        });
+    }
+  }
+
+  fetch(url) {
+    return fetch(url);
   }
 }
 
 let sem = new Semaphore(2);
 
-sem.callFunction(fetch, "https://www.google.com");
+sem.callFunction("fetch", "https://www.google.com");
+sem.callFunction("fetch", "https://www.facebook.com");
+sem.callFunction("fetch", "https://www.amazon.com");
+sem.callFunction("fetch", "https://www.netflix.com");
